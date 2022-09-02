d3ebb88e7d3fb46ebbec163d451a764429749b34
a-touman <82515232+a-touman@users.noreply.github.com>
2022-09-03 00:06:33 +0300

    Add files via upload
    put api key here

---
+++ lib/utilities/api_keys.dart
@@ -0,0 +1,7 @@
+const String kAPI = "YOUR_API_KEY_HERE";
+const String X_RapidAPI_Host = "moviesminidatabase.p.rapidapi.com";
+const Map<String, String> kHeaders = {
+  "X-RapidAPI-Key": kAPI,
+  "X-RapidAPI-Host": X_RapidAPI_Host,
+  "useQueryString": "true"
+};
